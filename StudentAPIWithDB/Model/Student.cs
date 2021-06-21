691df57a4843c95792fe5d81a4c5f8571d18d203
NAZIA MAJEED <NAZIA MAJEED@PCHOME>
2021-06-21 18:09:59 +0500

    Add project files.

---
+++ StudentAPIWithDB/Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAPIWithDB.Model
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int rollNo { get; set; }
+        public int marks { get; set; }
+    }
+}
